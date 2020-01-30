543e2623e9e6462234308b5df6a134dd172eec00
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-30 19:54:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,15 +2,13 @@
 
 class Dog 
   def name=(dogs_name)
-    @this_dogs_name = dogs_name 
+    @name = dogs_name 
   end
   def name 
-    @this_dogs_name
+    return @name
   end
 end
 
-class Dog 
-  def bark
-    puts "woof!"
-  end
-end
+dog1 = Dog.new();
+
+puts dog1.name
